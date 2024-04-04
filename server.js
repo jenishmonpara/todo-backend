4b353c59fc3f19003cabead8d33e947ab1c41107
jenishmonpara <jenishmonpara@gmail.com>
2024-04-04 19:51:08 +0530

    basic express skeleton

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config()
+
+//express app
+const app = express();
+
+
+// middleware
+app.use((req, res, next) => {
+    console.log();
+    next();
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg : "Welcome to my app"});
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ' + process.env.PORT);
+});
+
+console.log('This line here.');
