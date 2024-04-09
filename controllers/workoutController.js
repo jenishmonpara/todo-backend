7cb1f0d00797ce31f24c98595cf394112b174151
jenishmonpara <jenishmonpara@gmail.com>
2024-04-09 19:51:49 +0530

    created all controllers and backend

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -39,13 +39,47 @@ const createWorkout = async (req,res) => {
 }
 
 // delete a workout
+const deleteWorkout = async (req, res) => {
+    const {id} = req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error : "no such workout"});
+        return;
+    }
+
+    const workout = await Workout.findOneAndDelete({_id : id});
+    if(!workout){
+        res.status(404).json({error : "no such workout"});
+    }
+    else{
+        res.status(200).json([workout,{mssg : "Workout Deleted"}]);
+    }
+}
 
 // update a workout
+const updateWorkout = async (req, res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error : "no such workout"});
+        return;
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id : id},{...req.body});
+
+    if(!workout){
+        res.status(404).json({error : "no such workout"});
+    }
+    else{
+        res.status(200).json([workout,{mssg : "Workout Updated"}]);
+    }
+}
 
 
 module.exports = {
     createWorkout,
     getWorkouts,
-    getWorkout
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
 }
