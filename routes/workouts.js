1978f2b053663a7cdb847ec2848770653b176271
jenishmonpara <jenishmonpara@gmail.com>
2024-04-09 19:11:52 +0530

    seperatted the controller logic

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const Workout = require('../models/workoutModel')
-
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+} = require('../controllers/workoutController')
 const  router = express.Router();
 
 // GET all workouts
-router.get('/', (req, res) => {
-    res.json({mssg : 'Get all the workouts'});
-});
+router.get('/', getWorkouts);
 
 // GET single workout
-router.get('/:id', (req, res) => {
-    res.json({mssg : 'Get workout with  id'});
-});
+router.get('/:id', getWorkout);
 
 //  POST a new workout
-router.post('/', async (req, res) => {
-    const {title,load,reps} = req.body;
-
-    try{
-        const workout = await Workout.create({title, load, reps});
-        res.status(200).json(workout);
-    }catch(error){
-        res.status(400).json({error : error.message});
-    };
-})
+router.post('/', createWorkout);
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
